fb56badd788230517c7bf58d602b29fae6c32238
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-10-27 15:59:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const kilometri = prompt("inserisci il chilometraggio")
+const soldi =  ( 0.21 * parseInt(kilometri) )
+console.log(soldi)
+const eta = prompt("inserisci la tua eta!")
+if (eta <= 17){
+  const sconto = math.floor((soldi/20)*100)
+  console.log(`il costo totale del tuo biglietto è: ${sconto}€`)
+}
+else if (eta >= 65){
+    const scontoOver = Math.floor(soldi/)
+}
